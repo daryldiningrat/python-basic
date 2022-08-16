35d36f1eae7e39847e1c5c46d9ab93556296dfda
Daryl Diningrat <diningratdaryl@gmail.com>
2022-08-17 00:07:35 +0700

    fix: comprehension

--- functional_programming/dict_comprehension.py
+++ functional_programming/dict_comprehension.py
@@ -15,5 +15,8 @@ my_dict = {k:v**4 for k, v in simple_dict.items()}
 my_dict2 = {k:v**4 for k, v in simple_dict.items()
             if v%2 ==0}
 
+my_dict3 = {num:num**4 for num in [1,2,3]}
+
 print(my_dict)
 print(my_dict2)
+print(my_dict3)
